cf975361cae941945bc095d7d3eb288eaa688a49
ilhamsena <32926512+ilhamsena@users.noreply.github.com>
2017-11-23 11:13:28 +0700

    Update Revisi1.c

--- Revisi1.c
+++ Revisi1.c
@@ -18,7 +18,13 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 	sprintf(fpath,"%s%s",dirpath,path);
 	res = lstat(fpath, stbuf);
 
-	if (res == -1)*buf, fuse_fill_dir_t filler,
+	if (res == -1)return -errno;
+
+	return 0;
+}
+
+		
+static int xmp_readdir(const char *path, void*buf, fuse_fill_dir_t filler,
 		       off_t offset, struct fuse_file_info *fi)
 {
   	char fpath[1000];
